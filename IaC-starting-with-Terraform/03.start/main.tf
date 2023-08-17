4d3dc86d5f76f7bbef828f74b90495a7ce078d37
bokdoll <winnerhj1109@naver.com>
2023-08-17 15:44:44 +0900

    add terraform block configuration

--- IaC-starting-with-Terraform/03.start/main.tf
+++ IaC-starting-with-Terraform/03.start/main.tf
@@ -3,8 +3,21 @@
 # ${path.module} ?
 # - 실행되는 테라폼 모듈의 파일 시스템 경로.
 
+terraform {
+  required_version = ">=1.0.0"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = ">=2.0.0"
+    }
+  }
+  backend "local" {
+    path = "state/terraform.tfstate"
+  }
+}
+
 resource "local_file" "abc" {
-  content  = "abc!"
+  content  = "123456!"
   filename = "${path.module}/abc.txt"
 }
 
